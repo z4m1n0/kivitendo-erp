76f86921abc6c461f0d2523f2bfe122f388de794
Hans Peter Schlaepfer <hanspeter.schlaepfer@revamp-it.ch>
2019-01-26 17:51:12 +0100

    Design 4.0: js: CustomerVendor: Bild für Link auf Karte (google maps) geändert

--- js/kivi.CustomerVendor.js
+++ js/kivi.CustomerVendor.js
@@ -146,7 +146,7 @@ namespace('kivi.CustomerVendor', function(ns) {
       $widgetWrapper = $(widgetWrapper);
 
       $widgetWrapper
-        .html('<img src="image/map.png" alt="'+ kivi.t8("Map") +'" title="'+ kivi.t8("Map") +'" />')
+        .html('<img src="image/map-earth-link.png" alt="'+ kivi.t8("Map") +'" title="'+ kivi.t8("Map") +'" />')
         .click(function() {
           showMap();
         });
