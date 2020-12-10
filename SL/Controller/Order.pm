f9210dae67f7b5f868399b4371d2595abf707047
Cem Aydin <cem.aydin@gmx.ch>
2020-12-10 20:36:03 +0100

    Neues Design Bugfix: Anzahl Spalten Positionstabellen Angebot/Auftrag korrigiert
    Wenn Umsatzsteuer aktiv müssen die Zeilen Steuern und Summen, bei 
    Auftrag und Lieferantenauftrag, weiter eingerückt werden.

--- SL/Controller/Order.pm
+++ SL/Controller/Order.pm
@@ -1278,7 +1278,12 @@ sub build_tax_rows {
 
   my $rows_as_html;
   foreach my $tax (sort { $a->{tax}->rate cmp $b->{tax}->rate } @{ $self->{taxes} }) {
-    $rows_as_html .= $self->p->render('order/tabs/_tax_row', TAX => $tax, TAXINCLUDED => $self->order->taxincluded);
+    $rows_as_html .= $self->p->render(
+      'order/tabs/_tax_row',
+      TAX => $tax,
+      TAXINCLUDED => $self->order->taxincluded,
+      ADD_COL => $self->order->quotation ? 0 : 1
+    );
   }
   return $rows_as_html;
 }
