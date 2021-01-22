05101e08a20efbabe9d76e02539dbe3e20d1d589
Sven Schöling <s.schoeling@googlemail.com>
2021-01-22 17:47:34 +0100

    Mobile Layout: Javascript initialisierung vereinfacht

--- SL/Layout/MaterialMenu.pm
+++ SL/Layout/MaterialMenu.pm
@@ -9,15 +9,6 @@ sub init_menu {
   SL::Menu->new('mobile');
 }
 
-sub javascripts_inline {
-<<'EOL';
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-EOL
-}
-
 sub pre_content {
   $_[0]->presenter->render('menu/material', menu => $_[0]->menu, C => SL::Controller::Base->new);
 }
