03f9894ba3a4ae5f922497e892f7e577669b563a
Sven Schöling <s.schoeling@googlemail.com>
2021-01-22 17:47:01 +0100

    Mobile Menu: Controller für urls

--- SL/Layout/MaterialMenu.pm
+++ SL/Layout/MaterialMenu.pm
@@ -3,6 +3,7 @@ package SL::Layout::MaterialMenu;
 use strict;
 use parent qw(SL::Layout::Base);
 use SL::Menu;
+use SL::Controller::Base;
 
 sub init_menu {
   SL::Menu->new('mobile');
@@ -18,7 +19,7 @@ EOL
 }
 
 sub pre_content {
-  $_[0]->presenter->render('menu/material', menu => $_[0]->menu);
+  $_[0]->presenter->render('menu/material', menu => $_[0]->menu, C => SL::Controller::Base->new);
 }
 
 1;
