73dcd174aa4f3c0625f901d452abd721f07ac6ee
Sven Schöling <s.schoeling@googlemail.com>
2021-01-22 17:41:17 +0100

    Layout::MobileLogin: content wrapper wird nicht benötigt

--- SL/Layout/MobileLogin.pm
+++ SL/Layout/MobileLogin.pm
@@ -5,14 +5,6 @@ use parent qw(SL::Layout::Base);
 use SL::Layout::MaterialStyle;
 use SL::Layout::MaterialMenu;
 
-sub start_content {
-  "<div id='login' class='login'>\n";
-}
-
-sub end_content {
-  "</div>\n";
-}
-
 sub get_stylesheet_for_user {
   # overwrite kivitendo fallback
   'css/material';
