0b7b8ecc0d4fcdb74593ebe47826bdc8dfb17410
Tamino <tamino.steinert@tamino.st>
2021-11-03 07:50:24 +0100

    Reclamation: add default settings

---
+++ sql/Pg-upgrade2/defaults_add_reclamation.sql
@@ -0,0 +1,14 @@
+-- @tag: defaults_add_reclamations
+-- @description: Add defaults show_delete, always_project, warn_no_reqdate and warn_duplicate_parts for reclamations
+-- @depends: reclamations
+
+--show_delete
+ALTER TABLE defaults ADD COLUMN sales_reclamation_show_delete    boolean NOT NULL DEFAULT TRUE;
+ALTER TABLE defaults ADD COLUMN purchase_reclamation_show_delete boolean NOT NULL DEFAULT TRUE;
+
+--project
+ALTER TABLE defaults ADD COLUMN reclamation_always_project       boolean NOT NULL DEFAULT FALSE;
+
+--warn
+ALTER TABLE defaults ADD COLUMN reclamation_warn_no_reqdate      boolean NOT NULL DEFAULT TRUE;
+ALTER TABLE defaults ADD COLUMN reclamation_warn_duplicate_parts boolean NOT NULL DEFAULT TRUE;
